4d807c2651d90415d4769dc10d95965500794ca0
jgalgarra <jgalgarra@coit.es>
2014-09-06 13:04:40 +0200

    Minor form design issues fixed 3

--- src/pak_tfm/bino_form.py
+++ src/pak_tfm/bino_form.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'bino_form.ui'
 #
-# Created: Sat Sep  6 13:02:17 2014
+# Created: Sat Sep  6 13:04:23 2014
 #      by: PyQt4 UI code generator 4.9.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -154,7 +154,7 @@ class Ui_Form(object):
         self.label_report.setText(_fromUtf8(""))
         self.label_report.setObjectName(_fromUtf8("label_report"))
         self.URL_report = QtGui.QLabel(Form)
-        self.URL_report.setGeometry(QtCore.QRect(220, 510, 551, 40))
+        self.URL_report.setGeometry(QtCore.QRect(200, 510, 581, 40))
         self.URL_report.setText(_fromUtf8(""))
         self.URL_report.setOpenExternalLinks(True)
         self.URL_report.setObjectName(_fromUtf8("URL_report"))
